4af24a5ebc2e41d9febc2140ce735f3a9fb6ef0c
raoul <jraoul2002@gmail.com>
2020-12-28 11:38:50 +0100

    all files modified

---
+++ src/handlers/handleUpdate.js
@@ -0,0 +1,6 @@
+'use strict';
+
+export  function handleUpdate() {
+  const suffix = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
---
+++ src/listeners/change.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import { handleUpdate } from '../handlers/handleUpdate.js';
+
+
+const inputs = document.querySelectorAll('.controls input');
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
